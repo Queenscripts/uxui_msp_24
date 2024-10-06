c49c97139ad08abaf697994a59262b4f647e0929
Queen <queennefertitishabazz@gmail.com>
2024-10-06 13:18:08 -0700

    debugapi5

--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -46,6 +46,6 @@ export default async function handler(req:any, res:any) {
       return res.status(500).json({ error: 'Internal server error' });
     }
   }
-    return res.status(405).end();
+    return res.status(200).end();
 
 }
