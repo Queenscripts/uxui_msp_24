b1a3f6039b7f03df888628298efe557fc93e5ce1
Queen <queennefertitishabazz@gmail.com>
2025-03-05 06:55:05 -0800

    Update 21.tsx

--- src/pages/week/21.tsx
+++ src/pages/week/21.tsx
@@ -48,7 +48,7 @@ const One = () => {
       }
       slidesLinks={[
         `https://docs.google.com/presentation/d/e/2PACX-1vSKF9USDDRrSpp6k2goeWOsSbcdafZOrOB95PJRC-DnQuVhyGM7-FeCZRrJcYNHeaObUaAcrYDcvpxO/embed?start=false&loop=false&delayms=10000`,
-        `htps://docs.google.com/presentation/d/e/2PACX-1vTfv64csgBvUvTzcu6QGNwFs1hH1q-2M_uOg3zYVQWdMp4O8Tatfv9ZAnHK6CIge_XWS39puS9jrSyz/embed?start=false&loop=false&delayms=10000`
+        `https://docs.google.com/presentation/d/e/2PACX-1vTfv64csgBvUvTzcu6QGNwFs1hH1q-2M_uOg3zYVQWdMp4O8Tatfv9ZAnHK6CIge_XWS39puS9jrSyz/embed?start=false&loop=false&delayms=10000`
             ]}      
       slideURL="https://drive.google.com/drive/folders/1anv0mUPCZEpo1v_74jtvRyLATucu0hZK?usp=sharing" 
 
