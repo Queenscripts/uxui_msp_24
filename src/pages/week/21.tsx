886322dd1ebca3bbb215f0d1dbc04f320b07dd64
Queen <queennefertitishabazz@gmail.com>
2025-03-06 16:28:54 -0800

    Update 21.tsx

--- src/pages/week/21.tsx
+++ src/pages/week/21.tsx
@@ -11,17 +11,6 @@ import BriefcaseVariantOutline from 'mdi-material-ui/BriefcaseVariantOutline'
 // ** Custom Components Imports
 import CardStatisticsVerticalComponent from 'src/@core/components/card-statistics/card-stats-vertical'
 
-// ** Styled Component Import
-// import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
-
-// ** Demo Components Imports
-// import Table from 'src/views/dashboard/Table'
-// import Trophy from 'src/views/dashboard/Trophy'
-// import TotalEarning from 'src/views/dashboard/TotalEarning'
-// import StatisticsCard from 'src/views/dashboard/StatisticsCard'
-// import WeeklyOverview from 'src/views/dashboard/WeeklyOverview'
-// import DepositWithdraw from 'src/views/dashboard/DepositWithdraw'
-// import SalesByCountries from 'src/views/dashboard/SalesByCountries'
 import CardNavigation from 'src/views/cards/CardNavigation'
 import ProtectedRoute from 'src/components/ProtectedRoute'
 import React from 'react'
